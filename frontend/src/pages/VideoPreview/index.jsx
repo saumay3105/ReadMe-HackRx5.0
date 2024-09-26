aa4fd039470d42468c35c9405656fdd4dc46d7c0
saumay3105 <112512912+saumay3105@users.noreply.github.com>
2024-09-26 18:49:43 +0530

    bg in video_preview

--- frontend/src/pages/VideoPreview/index.jsx
+++ frontend/src/pages/VideoPreview/index.jsx
@@ -73,6 +73,10 @@ function VideoPreview() {
 
   return (
     <div className="video-preview-container">
+      <video autoPlay muted loop className="background-video">
+          <source src="/bg3.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+      </video>
       <Header showNav={false} />
       <h1>Video Preview</h1>
 
